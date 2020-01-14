33d61312d7c84d1df3e762d9ea55889ade43e30f
Marten <mjnstag@gmail.com>
2020-01-14 10:08:45 +0000

    added comments to question.py, added small delay to html reload

--- question.py
+++ question.py
@@ -3,15 +3,23 @@ from random import shuffle
 import html
 
 def get_question():
-    temp = requests.get("https://opentdb.com/api.php?amount=2&type=multiple").json()['results'][0]
-    print(requests.get('https://opentdb.com/api_category.php'))
+    # Get question data from api
+    question_data = requests.get("https://opentdb.com/api.php?amount=2&type=multiple").json()['results'][0]
 
-    question = html.unescape(temp['question'])
-    correct_answer = html.unescape(temp['correct_answer'])
-    incorrect_answers = [html.unescape(i) for i in temp['incorrect_answers']]
+    # Get question and decodes keys
+    question = html.unescape(question_data['question'])
+    # Gets correct answer of question and decodes keys
+    correct_answer = html.unescape(question_data['correct_answer'])
+    # Puts all incorrect answers in a list and decodes them
+    incorrect_answers = [html.unescape(i) for i in question_data['incorrect_answers']]
+    # Makes list of all possible answers
     all_answers = [correct_answer] + incorrect_answers
+    # Randomize answer order
     shuffle(all_answers)
 
+    # remove when done
+    # print correct answer for testing purposes
     print(correct_answer)
 
+    # return question data to page
     return question, all_answers, correct_answer
